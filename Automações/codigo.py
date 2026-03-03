b0e8dc4f97c5004206f18f033456727d6b23f4e4
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2026-03-02 23:45:24 -0300

    Update codigo.py

--- Automações/codigo.py
+++ Automações/codigo.py
@@ -1,1 +1,29 @@
 
+
+# pip install pyautogui
+# pyautogui.press("win,space,command,tab,esc,etc) --> Pressionar uma tecla
+# pyautogui.click("enter, x=55, y=66")
+# pyautogui.hotkey("ctrl","l")
+# pyautogui.write("Texto qualquer")
+# pyautogui.PAUSE(0.5) - ou utilizar a lib "time";
+# print(pyautogui.position()) - Pega a posição que o mouse está;
+# é sempre bom utilizar um tempo de espera interessante para o site não entender que é um bot ou span.
+
+import pyautogui
+import time
+
+def buscarMusic():
+    pyautogui.press("win")
+    pyautogui.write("Google")
+    time.sleep(1.5)
+    pyautogui.press("enter")
+    time.sleep(1.5)
+    pyautogui.write("YouTube")
+    pyautogui.press("enter")
+    time.sleep(3)
+    pyautogui.click(x=642, y=146)
+    time.sleep(1.5)
+    pyautogui.write("Lembrei de tu")
+    pyautogui.press("enter")
+buscarMusic()
+
